6c3d879d1dd3a5d3ab0971652e232b1390db253f
“xionghongjie” <xionghongjie@kemai.cn>
2025-03-22 21:18:05 +0800

    first commit

---
+++ src/components/ShiftingDropdown/components/Tab.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { ChevronDown } from 'lucide-vue-next'
+defineProps<{
+  selected: number | null
+  tabId: number
+  handleTabFocus: (val: number | null) => void
+}>()
+
+</script>
+
+<template>
+  <button :id="`shift-tab-${tabId}`" @click="handleTabFocus(tabId)" @mouseenter="handleTabFocus(tabId)"
+    class="flex items-center gap-1 rounded-full px-3 py-1.5 text-sm transition-colors cursor-pointer" :class="{
+      'bg-neutral-800 text-neutral-100': selected === tabId,
+      'text-neutral-400': selected !== tabId
+    }">
+    <span>
+      <slot></slot>
+    </span>
+    <ChevronDown class="transition-transform" :class="{ 'rotate-180': selected === tabId }" :size="20"
+      :stroke-width="1.5"></ChevronDown>
+  </button>
+</template>
+
+<style scoped></style>
---
+++ src/components/ShiftingDropdown/index.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import Tabs from './components/Tabs.vue'
+
+</script>
+
+<template>
+  <div class="flex h-dvh pt-20 w-full justify-center bg-neutral-950 p-8 text-neutral-200 md:justify-center">
+    <Tabs />
+  </div>
+</template>
+
+<style scoped></style>
